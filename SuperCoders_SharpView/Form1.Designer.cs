0f0dab40b176cddcb986270042e0112fed3ba112
Keri <79213053+KeriGabriel@users.noreply.github.com>
2021-02-18 10:17:55 -0800

    button

--- SuperCoders_SharpView/Form1.Designer.cs
+++ SuperCoders_SharpView/Form1.Designer.cs
@@ -30,23 +30,36 @@ namespace SuperCoders_SharpView
         private void InitializeComponent()
         {
             this.randombtn = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // randombtn
             // 
-            this.randombtn.Location = new System.Drawing.Point(570, 223);
+            this.randombtn.Location = new System.Drawing.Point(760, 274);
+            this.randombtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.randombtn.Name = "randombtn";
-            this.randombtn.Size = new System.Drawing.Size(75, 23);
+            this.randombtn.Size = new System.Drawing.Size(100, 28);
             this.randombtn.TabIndex = 0;
             this.randombtn.Text = "test";
             this.randombtn.UseVisualStyleBackColor = true;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(272, 114);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.randombtn);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -56,6 +69,7 @@ namespace SuperCoders_SharpView
         #endregion
 
         private System.Windows.Forms.Button randombtn;
+        private System.Windows.Forms.Button button1;
     }
 }
 
