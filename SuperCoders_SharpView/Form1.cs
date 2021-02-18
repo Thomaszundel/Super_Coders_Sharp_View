4ab94d8f44139d77616cf2bbcb60e87bbd19f33a
Keri <79213053+KeriGabriel@users.noreply.github.com>
2021-02-18 15:36:02 -0800

    intitial set up

--- SuperCoders_SharpView/Form1.cs
+++ SuperCoders_SharpView/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace SuperCoders_SharpView
 {
-    public partial class Form1 : Form
+    public partial class FormSharpView : Form
     {
-        public Form1()
+        public FormSharpView()
         {
             InitializeComponent();
         }
--- SuperCoders_SharpView/Program.cs
+++ SuperCoders_SharpView/Program.cs
@@ -16,7 +16,7 @@ namespace SuperCoders_SharpView
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormSharpView());
         }
     }
 }
