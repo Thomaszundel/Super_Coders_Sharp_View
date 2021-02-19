0fffd755afcec0dc5eef868efa5592ba330454dc
IanSchow <ianmschow@gmail.com>
2021-02-19 11:48:51 -0700

    Added Splash Screen that plays Karlson Vibe for 5 seconds before launching the program

--- SuperCoders_SharpView/Program.cs
+++ SuperCoders_SharpView/Program.cs
@@ -16,6 +16,7 @@ namespace SuperCoders_SharpView
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new SplashScreen());
             Application.Run(new FormSharpView());
         }
     }
